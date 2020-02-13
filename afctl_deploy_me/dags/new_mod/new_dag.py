4282eb714b1989ae530bf8a3b363a46ca32b4df4
Aaditya Sharma <asharma@qubole.com>
2020-02-13 11:38:09 +0530

    Initial commit

--- afctl_deploy_me/dags/new_mod/new_dag.py
+++ afctl_deploy_me/dags/new_mod/new_dag.py
@@ -4,13 +4,14 @@ from datetime import datetime, timedelta
 from airflow.operators.dummy_operator import DummyOperator
 
 default_args = {
-'owner': 'afctl_deploy_me'
+'owner': 'afctl_deploy_me',
+'start_date': datetime.now()
 }
 
 dag = DAG(dag_id='new', default_args=default_args, schedule_interval='@once')
 
-task_1 = DummyOperator('task_1', dag=dag)
-task_2 = DummyOperator('task_2', dag=dag)
+task_1 = DummyOperator(task_id='task_1', dag=dag)
+task_2 = DummyOperator(task_id='task_2', dag=dag)
 
 task_1 >> task_2
 
